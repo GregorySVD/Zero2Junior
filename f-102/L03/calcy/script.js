94d677417d0f288bb6c6d89b417527d6de4096f0
grzeg <grzegorzterenda@gmail.com>
2024-03-23 18:32:20 +0100

    moved all mathemathics operation to separate method

--- f-102/L03/calcy/script.js
+++ f-102/L03/calcy/script.js
@@ -2,39 +2,57 @@ const numberA = Number(prompt("Podaj pierwsza liczbe"));
 const action = prompt("Podaj operator arytmetyczny (+,-,/,* lub %)")
 const numberB = Number(prompt("Podaj druga liczbe"));
 
+//MATHEMATICS OPERATIONS
+function add(numberA, numberB) {
+    return numberA + numberB;
+}
 
-let result = null;
-let errorMessage = '';
+function subtract(numberA, numberB) {
+    return numberA - numberB;
+}
+
+function multiply(numberA, numberB) {
+    return numberA * numberB;
+}
+
+function divide(numberA, numberB) {
+    if (numberB === 0) {
+        return "Nie mozna dzielic przez zero.";
+    }
+    return numberA / numberB;
+}
+
+function modulo(numberA, numberB) {
+    if (numberB === 0) {
+        return "nie ma cechy podzielności przez 0, ponieważ żadna liczba nie dzieli się przez 0.";
+    }
+    return numberA % numberB;
+}
+//----------------------------------------------------------------
+
+let result = 0;
 
 if (isNaN(numberA) || isNaN(numberB)) {
     alert("Podane wartości muszą być liczbami😊.");
 } else {
     switch (action) {
         case "+" :
-            result = numberA + numberB;
+            result = add(numberA, numberB);
             break;
         case "*" :
-            result = numberA * numberB;
+            result = multiply(numberA, numberB);
             break;
         case "-" :
-            result = numberA - numberB;
+            result = subtract(numberA, numberB);
             break;
         case "%" :
-            if (numberB === 0) {
-                errorMessage = "nie ma cechy podzielności przez 0, ponieważ żadna liczba nie dzieli się przez 0.";
-                break;
-            }
-            result = numberA % numberB;
+            result = modulo(numberA, numberB);
             break;
         case "/" :
-            if (numberB === 0) {
-                errorMessage = "Nie mozna dzielic przez zero.";
-                break;
-            }
-            result = numberA / numberB;
+            result = divide(numberA, numberB);
             break;
         default:
-            errorMessage("Nieoczekiwany błąd");
+            result = "Nieoczekiwany błąd";
     }
-    alert(result === null ? errorMessage : result);
+    alert(result);
 }
